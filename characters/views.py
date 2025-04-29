007c5577cf112c0d857b0e0f55ce118ae33113c3
jess-ar <jessica18_24@hotmail.com>
2025-04-29 22:09:03 +0200

    Enable CORS_ALLOW_ALL_ORIGINS for development

--- characters/views.py
+++ characters/views.py
@@ -9,6 +9,7 @@ from rest_framework.pagination import PageNumberPagination
 
 class CharacterSearchView(generics.ListAPIView):
     serializer_class = CharacterSerializer
+    authentication_classes = []
     permission_classes = [AllowAny]
 
     def get_queryset(self):
--- marvel_dc_ultimate_guide/settings.py
+++ marvel_dc_ultimate_guide/settings.py
@@ -184,6 +184,15 @@ ALLOWED_HOSTS = [
 '127.0.0.1',
 ]
 
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+]
+
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     'http://localhost:5173',
